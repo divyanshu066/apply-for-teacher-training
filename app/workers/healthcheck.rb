1f7b73b6f99e75c176d556034b68a911c28e57cc
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-04-24 19:00:44 +0100

    string ' instead of "

--- app/workers/healthcheck.rb
+++ app/workers/healthcheck.rb
@@ -2,7 +2,7 @@ class HealthCheck
   include Sidekiq::Worker
 
   def perform(*)
-    Rails.logger.info "APP_MONITORING_ENDPOINT " + ENV['APP_MONITORING_ENDPOINT']
+    Rails.logger.info 'APP_MONITORING_ENDPOINT ' + ENV['APP_MONITORING_ENDPOINT']
     response = HTTP.get(ENV['APP_MONITORING_ENDPOINT'])
     Rails.logger.info 'Application is running...' if response.status.success?
   end
