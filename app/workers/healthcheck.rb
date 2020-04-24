c7382bbed63c98defc3526fac55b0a7eff468e7f
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-04-24 17:33:49 +0100

    class name

--- app/workers/healthcheck.rb
+++ app/workers/healthcheck.rb
@@ -1,8 +1,8 @@
-class ClockworkCheck
+class HealthCheck
   include Sidekiq::Worker
 
   def perform(*)
-    Rails.logger.info ENV['APP_MONITORING_ENDPOINT']
+    Rails.logger.info "APP_MONITORING_ENDPOINT " + ENV['APP_MONITORING_ENDPOINT']
     response = HTTP.get(ENV['APP_MONITORING_ENDPOINT'])
     Rails.logger.info 'Application is running...' if response.status.success?
   end
