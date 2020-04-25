281335d402373b328c653c16603126507ed85d43
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-04-25 16:18:01 +0100

    lint error

--- app/workers/health_check_worker.rb
+++ app/workers/health_check_worker.rb
@@ -8,11 +8,11 @@ class HealthCheckWorker
 
     @health_check_url = ENV['APP_MONITORING_ENDPOINT']
 
-    unless @health_check_url.present?
+    if @health_check_url.blank?
       Rails.logger.info 'health_check_url not present'
       raise 'health_check_url not present'
     end
- 
+
     Rails.logger.info "APP_MONITORING_ENDPOINT  #{@health_check_url}"
     response = HTTP.get(@health_check_url)
     Rails.logger.info 'Application is running...' if response.status.success?
