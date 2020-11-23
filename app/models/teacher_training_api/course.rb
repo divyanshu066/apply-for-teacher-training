60f9f6fc6a74b302d4302afc8ce2f16ab7bfa50f
Duncan Brown <duncanjbrown@gmail.com>
2020-11-23 16:30:14 +0000

    Configure JSON API for new param names in the TeacherTrainingAPI
    Optimistically remove some code we didn’t understand while we’re here

--- app/models/teacher_training_api/course.rb
+++ app/models/teacher_training_api/course.rb
@@ -1,10 +1,10 @@
 module TeacherTrainingAPI
   class Course < TeacherTrainingAPI::Resource
-    belongs_to :recruitment_cycle, through: :provider, param: :recruitment_cycle_year
+    belongs_to :recruitment_cycle, through: :provider, param: :year
     belongs_to :provider, param: :provider_code
 
     def self.fetch(provider_code, course_code)
-      where(recruitment_cycle_year: RecruitmentCycle.current_year)
+      where(year: RecruitmentCycle.current_year)
         .where(provider_code: provider_code)
         .find(course_code)
         .first
