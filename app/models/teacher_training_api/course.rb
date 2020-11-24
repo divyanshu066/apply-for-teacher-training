01af494439c8079404625349ca122d618a843a33
Duncan Brown <duncanjbrown@gmail.com>
2020-11-24 12:00:55 +0000

    Parameterize recruitment_cycle_year in TTAPI::Course.fetch
    Less dependency on state from elsewhere makes this method simpler

--- app/models/teacher_training_api/course.rb
+++ app/models/teacher_training_api/course.rb
@@ -3,8 +3,8 @@ module TeacherTrainingAPI
     belongs_to :recruitment_cycle, through: :provider, param: :year
     belongs_to :provider, param: :provider_code
 
-    def self.fetch(provider_code, course_code)
-      where(year: RecruitmentCycle.current_year)
+    def self.fetch(recruitment_cycle_year: RecruitmentCycle.current_year, provider_code:, course_code:)
+      where(year: recruitment_cycle_year)
         .where(provider_code: provider_code)
         .find(course_code)
         .first
