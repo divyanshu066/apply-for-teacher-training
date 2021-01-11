03e54ff15e9c35b59e88d37abbdbe489eff0bfb1
benswannack <ben.swannack@softwire.com>
2021-01-11 13:38:39 +0000

    Work in progress - Add interviews create flow

---
+++ app/controllers/provider_interface/interviews_controller.rb
@@ -0,0 +1,15 @@
+module ProviderInterface
+  class InterviewsController
+    def new
+      #  render the form
+    end
+
+    def create
+      #  Create the interview
+    end
+
+    def check
+      #  Check answers
+    end
+  end
+end
