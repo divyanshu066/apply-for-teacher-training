825b1d2e45cf33ad60f40b002fcf041fd693877c
benswannack <ben.swannack@softwire.com>
2021-01-12 11:47:28 +0000

    Add some form validations and specs for them

--- app/controllers/provider_interface/interviews_controller.rb
+++ app/controllers/provider_interface/interviews_controller.rb
@@ -1,24 +1,51 @@
 module ProviderInterface
   class InterviewsController < ProviderInterfaceController
     before_action :set_application_choice
+
     def new
       @new_interview_form = NewInterviewForm.new
+    end
+
+    def check
+      @new_interview_form = NewInterviewForm.new(new_interview_params)
+
+      render :new unless @new_interview_form.valid?
     end
 
     def create
       #  Create the interview
     end
 
-    def check
-      #  Check answers
+  private
+
+    def make_decisions_permission_orgs
+      @_make_decisions_permission_orgs ||= begin
+        application_choice_providers = [@application_choice.provider, @application_choice.accredited_provider].compact
+        current_user_providers = current_provider_user
+          .provider_permissions
+          .make_decisions
+          .map(&:provider)
+
+        current_user_providers.select { |provider| application_choice_providers.include?(provider)  }
+      end
+    end
+    helper_method :make_decisions_permission_orgs
+
+    def new_interview_params
+      params
+        .require(:provider_interface_new_interview_form)
+        .permit(:'date(3i)', :'date(2i)', :'date(1i)', :time)
+        .transform_keys { |key| date_field_to_attribute(key) }
+        .transform_values(&:strip)
     end
 
-    def has_multiple_interviewer_options?
-      application_choice.accredited_provider.present? && current_provider_user.provider_permissions.make_decisions.exists?(provider: provider) && current_provider_user.provider_permissions.make_decisions.exists?(provider: accredited_provider)
-    end
-
-    def show_organisations_user_has_permissions_for
-      
+    def date_field_to_attribute(key)
+      case key
+      when 'date(3i)' then 'day'
+      when 'date(2i)' then 'month'
+      when 'date(1i)' then 'year'
+      else key
+      end
     end
   end
 end
--- app/forms/provider_interface/new_interview_form.rb
+++ app/forms/provider_interface/new_interview_form.rb
@@ -1,6 +1,30 @@
 module ProviderInterface
   class NewInterviewForm
     include ActiveModel::Model
-    
+
+    attr_accessor :day, :month, :year, :time
+
+    validate :date_is_valid
+    validate :time_is_valid
+    validate :date_and_time_in_future, if: -> form { form.date_is_valid && form.time_is_valid }
+
+    def date_and_time
+      Date.new()
+    end
+
+    def date_is_valid
+      date_args = [year, month, day].map(&:to_i)
+      binding.pry
+      errors[:date] << 'Enter a date' unless Date.valid_date?(*date_args)
+    end
+
+    def time_is_valid
+      # TODO: Validation to check this is valid!
+      errors[:date] << 'Enter a time' unless time.blank?
+    end
+
+    def date_and_time_in_future
+      errors[:time] << 'Enter a date and time in the future' if date_and_time < Time.zone.now
+    end
   end
 end
